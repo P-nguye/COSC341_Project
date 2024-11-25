16f162a669cc6e3b2bcae9a019df7cca764cd6b5
TobyNguyen710 <ductoby2003@gmail.com>
2024-11-25 14:53:14 -0800

    Implemented Plant and Crop Tracker task

--- app/src/main/java/com/example/cosc341_project/MainActivity.java
+++ app/src/main/java/com/example/cosc341_project/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.cosc341_project;
+//import com.example.group17_project.ExpandableListAdapter;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+//import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 public class MainActivity extends AppCompatActivity {
+    private ExpandableListView expandableListView;
+    private ExpandableListAdapter adapter;
+    private List<String> parentList;
+    private HashMap<String, List<String>> childList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +44,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void toTracker(View view){
+        Intent intent = new Intent(this, PlantCropTracker.class);
+        startActivity(intent);
     }
 }
